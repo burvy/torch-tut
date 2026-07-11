578ff71601929e63f8c8b0b10ceb45d93cb98efb
Burvye <thebuffonion@gmail.com>
2026-07-11 18:20:20 -0400

    fix: no more warnings
    Co-authored-by: Nube-Lube <103972748+Nube-Lube@users.noreply.github.com>

---
+++ src/neural_net.py
@@ -0,0 +1,26 @@
+import numpy as np
+import src.functions
+NN = [
+    {"input_dim": 2, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"},
+]
+
+def init_layers(neural_net, seed = 99):
+    np.random.seed(seed)
+    number_of_layers = len(neural_net)
+    params_values = {}
+
+    for idx, layer in enumerate(neural_net):
+        layer_idx = idx + 1
+        layer_input_size = layer["input_dim"]
+        layer_output_size = layer["output_dim"]
+
+        params_values['W' + str(layer_idx)] = np.random.randn(
+            layer_output_size, layer_input_size) * 0.1
+        params_values['b' + str(layer_idx)] = np.random.randn(
+            layer_output_size, 1) * 0.1
+
+    return params_values
