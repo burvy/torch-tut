8b003d5a08a0d4e312a2c5e7e63aa0c7c1b53a9b
Burvye <thebuffonion@gmail.com>
2026-07-11 18:33:14 -0400

    feat: tut items
    Co-authored-by: Nube-Lube <103972748+Nube-Lube@users.noreply.github.com>

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,6 +1,7 @@
 import numpy as np
-import src.functions
-NN = [
+import src.functions as fns
+
+NEURAL_NET = [
     {"input_dim": 2, "output_dim": 4, "activation": "relu"},
     {"input_dim": 4, "output_dim": 6, "activation": "relu"},
     {"input_dim": 6, "output_dim": 6, "activation": "relu"},
@@ -8,19 +9,34 @@ NN = [
     {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"},
 ]
 
-def init_layers(neural_net, seed = 99):
-    np.random.seed(seed)
-    number_of_layers = len(neural_net)
-    params_values = {}
+def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
+    Z_curr = np.dot(W_curr, A_prev) + b_curr
 
-    for idx, layer in enumerate(neural_net):
-        layer_idx = idx + 1
+    if activation is "relu":
+        activation_func = fns.relu
+    elif activation is "sigmoid":
+        activation_func = fns.sigmoid
+    else:
+        raise Exception('Non-supported activation function')
+
+    return activation_func(Z_curr), Z_curr
+
+
+def init_layers(neural_net, seed = 99):
+    np.random.seed(seed) # randomizes seed parameter in function
+    number_of_layers = len(neural_net) # length of neural net
+    params_values = {} # no values yet
+
+    for idx, layer in enumerate(neural_net): # for each layer in the network
+        layer_idx = idx + 1 # so its not 0 indexed?
         layer_input_size = layer["input_dim"]
         layer_output_size = layer["output_dim"]
 
         params_values['W' + str(layer_idx)] = np.random.randn(
-            layer_output_size, layer_input_size) * 0.1
+            layer_output_size, layer_input_size) * 0.1 # creates random weights
         params_values['b' + str(layer_idx)] = np.random.randn(
-            layer_output_size, 1) * 0.1
+            layer_output_size, 1) * 0.1 # creates some random biases
 
     return params_values
+
+print(init_layers(NEURAL_NET))
