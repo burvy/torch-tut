dffafa2ba650958c4b589381e607ae544498926c
Burvye <thebuffonion@gmail.com>
2026-07-12 06:48:19 -0400

    feat: dataset for nums
    Co-authored-by: Nube-Lube <103972748+Nube-Lube@users.noreply.github.com>

--- src/num_classifier.py
+++ src/num_classifier.py
@@ -1,8 +1,88 @@
 from mlxtend.data import loadlocal_mnist
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import random
+from os.path  import join
+import struct
+from array import array
 
-for dirname, _, filenames in os.walk('/kaggle/input'):
+
+for dirname, _, filenames in os.walk('/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+
+class MnistDataloader(object):
+    def __init__(self, training_images_filepath,training_labels_filepath,
+                 test_images_filepath, test_labels_filepath):
+        self.training_images_filepath = training_images_filepath
+        self.training_labels_filepath = training_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
+
+    def read_images_labels(self, images_filepath, labels_filepath):
+        labels = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            labels = array("B", file.read())
+
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = img.reshape(28, 28)
+            images[i][:] = img
+
+        return images, labels
+
+    def load_data(self):
+        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (x_train, y_train),(x_test, y_test)
+
+# Set file paths based on added MNIST Datasets
+training_images_filepath = '../input/train-images.idx3-ubyte'
+training_labels_filepath = '../input/train-labels.idx1-ubyte'
+test_images_filepath = '../input/t10k-images.idx3-ubyte'
+test_labels_filepath = '../input/t10k-labels.idx1-ubyte'
+
+
+# Load MINST dataset
+print('Loading MNIST dataset...')
+mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+print('MNIST dataset loaded.')
+
+# Show example images
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images)/cols) + 1
+    plt.figure(figsize=(28, 28))
+    index = 1
+    for x in zip(images, title_texts):
+        image = x[0]
+        title_text = x[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image, cmap=plt.cm.gray)
+        if (title_text != ''):
+            plt.title(title_text, fontsize = 15);
+        index += 1
+
+random_images = []
+for i in range(0, 10):
+    r = random.randint(1, 60000)
+    random_images.append((x_train[r], 'training image [' + str(r) + '] = ' + str(y_train[r])))
+for i in range(0, 5):
+    r = random.randint(1, 10000)
+    random_images.append((x_test[r], 'test image [' + str(r) + '] = ' + str(y_test[r])))
+
+show_images(list(map(lambda x: x[0], random_images)), list(map(lambda x: x[1], random_images)))
